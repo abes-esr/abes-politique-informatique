ed4d9ab363e06658e46d856dc30d604628068a70
Stéphane Gully <gully@abes.fr>
2025-10-03 14:51:42 +0200

    fix: remove trailing slash for sitemap

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # initialize default values for parameters
-export ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL=${ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL:='https://politique-informatique.abes.fr'}
+ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL=${ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL:='https://politique-informatique.abes.fr'}
+# remove optional trailing slash (do not want it)
+ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL="$(printf '%s' "$ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL" | sed 's:/*$::')"
+export ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL
 
 # patch sitemap because it needs to have the absolute url
 if [ ! -f /usr/share/nginx/html/sitemap.xml.orig ]; then
