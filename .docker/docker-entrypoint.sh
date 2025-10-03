ab8ebc4444040f3cefe67d5aedc252626ff6472d
Stéphane Gully <gully@abes.fr>
2025-10-03 14:36:52 +0200

    Update docker-entrypoint.sh

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # initialize default values for parameters
-export POLINFO_BASEURL=${POLINFO_BASEURL:='https://politique-informatique.abes.fr'}
+export ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL=${ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL:='https://politique-informatique.abes.fr'}
 
 # patch sitemap because it needs to have the absolute url
 if [ ! -f /usr/share/nginx/html/sitemap.xml.orig ]; then
   cp -f /usr/share/nginx/html/sitemap.xml /usr/share/nginx/html/sitemap.xml.orig
 fi
-sed "s#https://politique_informatique_abes_baseurl_placeholder#${POLINFO_BASEURL}#g" \
+sed "s#https://politique_informatique_abes_baseurl_placeholder#${ABES_POLITIQUE_INFORMATIQUE_HTTP_BASEURL}#g" \
   /usr/share/nginx/html/sitemap.xml.orig \
   > /usr/share/nginx/html/sitemap.xml
 
